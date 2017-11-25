b40fe2d8439525a76b1c95d957ffac53b032bccd
Adam Chambers <adammchambers@gmail.com>
2017-11-25 10:14:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DSComponent
+
+A command line utility to assist in the creation of DS Boilerplate components. Create a new component in your projects components directory with Sass and optional JS files ready to go.
+
+```
+# clone this repo
+$ git clone git@github.com:digitalsurgeons/dscomponent.git
+
+# install dependencies
+$ cd dscomponent
+$ npm i
+
+# install module globally
+$ npm i -g ./
+
+# run command from the root of any project
+$ dscomponent component-name  
+
+# pass optional --no-js argument for Sass only
+$ dscomponent component-name --no-js
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const program = require('commander')
+let data = {}
+
+program
+  .version('1.0.0')
+  .description(
+    'A command line utility to assist in the creation of DS Boilerplate component'
+  )
+  // program.js will be set to false if --no-js specified
+  .option('--no-js', 'Remove index.js')
+  // first and only argument is name of component
+  .arguments('<name>')
+  // store name in data object
+  .action(name => {
+    data.name = name
+  })
+  .parse(process.argv)
+
+// directory paths
+const parentDir = `${process.cwd()}/components`
+const childDir = `${parentDir}/${data.name}`
+
+// if no components folder then create first
+if (!fs.existsSync(parentDir)) {
+  fs.mkdirSync(parentDir)
+}
+
+// then create component directory
+if (!fs.existsSync(childDir)) {
+  fs.mkdirSync(childDir)
+}
+
+// create styles file
+fs.openSync(`${childDir}/styles.scss`, 'w')
+
+// create JS file if required
+if (program.js) {
+  fs.openSync(`${childDir}/index.js`, 'w')
+}
+
+console.log(`${data.name} component created`)
